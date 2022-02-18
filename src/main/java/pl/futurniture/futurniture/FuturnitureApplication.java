013ed4ba0959bd654a3f0b3bd09db54085f57244
Jakub Brózda <brozdajakub@gmail.com>
2022-02-18 11:52:18 +0100

    initial commit

---
+++ src/main/java/pl/futurniture/futurniture/FuturnitureApplication.java
@@ -0,0 +1,13 @@
+package pl.futurniture.futurniture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuturnitureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FuturnitureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/futurniture/futurniture/FuturnitureApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.futurniture.futurniture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuturnitureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
